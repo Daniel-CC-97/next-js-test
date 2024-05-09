2b113e86905d4f37519277d61390f5a669ff697b
Daniel Cassell <danielcassell@Daniels-MacBook-Pro.local>
2024-05-09 21:23:07 +0100

    Style changes

--- app/page.tsx
+++ app/page.tsx
@@ -9,22 +9,23 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        <AcmeLogo />
+      <div className="flex h-20 shrink-0 content-center items-end rounded-lg bg-slate-500 p-3 md:h-52">
+        {/* <AcmeLogo /> */}
+        <h1 className=' text-2xl font-extrabold text-white md:text-5xl'>Daniel's Super Cool Accounting Website</h1>
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
         <div className={styles.shape}/>
           <p className={` ${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
+            <strong>Welcome to Dan's Accounting.</strong>{' '}
+            {/* <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
-            </a>
-            , brought to you by Daniel.
+            </a> */}
+            This is a super cool accounting website that realistically does nothing at all.
           </p>
           <Link
             href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            className="flex items-center gap-5 self-start rounded-lg bg-slate-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
