0ae188171d61aa2f42fc0f61d440579f5b9c2578
Daniel Cassell <danielcassell@Daniels-MacBook-Pro.local>
2024-05-09 21:31:10 +0100

    Reset

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -19,7 +19,7 @@ export default async function Page() {
 
     return (
         <main>
-            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2x1 text-white`}>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2x1`}>
                 Dashboard
             </h1>
             <div className='grid gap-6 sm:grid-cols-2 lg:grid-cols-4'>
--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialised bg-black`}>{children}</body>
+      <body className={`${inter.className} antialised`}>{children}</body>
     </html>
   );
 }
